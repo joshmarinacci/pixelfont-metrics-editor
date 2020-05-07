8ae82e10655aa0db8e6b57f43901d9e4cb4756c1
josh <joshua@marinacci.org>
2020-05-07 15:56:43 -0700

    initila commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+let img = new Image()
+img.src = "BitScript.png"
+let stuff = {
+    offset:10,
+}
+let metrics = []
+for(let i=65; i<90; i++) {
+    metrics[i] = {num:i, x:(i-65)*8, y:0, w:8, h:8, blank:false, ch:String.fromCharCode(i)}
+}
+stuff.metrics = metrics
+
+const MetricsList = ({stuff, setGlobal, set})=>{
+    let metrics = stuff.metrics
+    console.log("rendering list")
+    return <ul>
+        <li>
+            <label>offset</label>
+            <input type="number" value={stuff.offset} onChange={(e)=>{
+                stuff.offset = parseInt(e.target.value)
+                setGlobal('offset',stuff.offset)
+            }}/>
+        </li>
+        {metrics.map((metrics,i) => {
+            return <li key={i}>
+                <label>{metrics.ch}</label>
+                <label>x</label>
+                <input type="number" value={metrics.x}
+                       onChange={(e)=>set(metrics.num,'x',parseFloat(e.target.value))}/>
+                <label>y</label>
+                <input type="number" value={metrics.y}/>
+                <label>w</label>
+                <input type="number" value={metrics.w}
+                       onChange={(e)=>set(metrics.num,'w',parseInt(e.target.value))}/>
+                />
+                <label>h</label>
+                <input type="number" value={metrics.h}/>
+            </li>
+        })}
+    </ul>
+}
+
+const MetricsCanvas = ({stuff,counter,sc})=>{
+    let ref = useRef()
+    useEffect(()=>{
+        if(ref.current) {
+            let ctx = ref.current.getContext('2d')
+            ctx.fillStyle = 'red'
+            // ctx.fillRect(0,0,30,30)
+            ctx.imageSmoothingEnabled = false
+
+            ctx.save()
+            ctx.scale(sc,sc)
+            ctx.drawImage(img,0,0)
+            ctx.restore()
+
+            ctx.save()
+
+            stuff.metrics.forEach(m => {
+                ctx.strokeStyle = 'yellow'
+                ctx.strokeRect((stuff.offset + m.x)*sc,
+                    m.y*sc,
+                    m.w*sc,
+                    m.h*sc)
+                ctx.fillStyle = 'magenta'
+                ctx.fillText(m.ch, (stuff.offset+m.x)*sc+10, 1*sc+10)
+            })
+
+            ctx.restore()
+        }
+    },[ref,counter, sc, stuff])
+    return <div className={'scroll'}>
+        <canvas ref={ref} width={600*sc} height={100}></canvas>
     </div>
+}
+
+
+const ExportPanel = ({})=>{
+    let [exportText,setExportText] = useState("foo")
+    return <div className={'vbox'}>
+        <button onClick={()=>{
+            console.log('exporting')
+            setExportText("foo2")
+        }}>export</button>
+        <textarea className={'export-area'}>{exportText}</textarea>
+    </div>
+}
+
+function App() {
+    let [m,setM] = useState(0)
+    let set = (num, prop, value) => {
+        console.log("setting",num,prop,value)
+        metrics[num][prop] = value
+        setM(m+1)
+    }
+    let setGlobal = (name) => {
+        setM(m+1)
+    }
+  return (
+      <div className="hbox">
+          <MetricsList stuff={stuff} set={set} setGlobal={setGlobal}/>
+          <div className={"vbox"}>
+              <MetricsCanvas stuff={stuff} counter={m} sc={10}/>
+              <ExportPanel stuff={stuff} counter={m}/>
+          </div>
+      </div>
   );
 }
+
+
 
 export default App;
